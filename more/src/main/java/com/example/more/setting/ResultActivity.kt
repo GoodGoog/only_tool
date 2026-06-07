6e6034d8d6d6e51350325dead16b4aa863c9e967
zhangqy <1213715120@qq.com>
2026-06-07 09:14:55 +0800

    基础功能搞定

--- more/src/main/java/com/example/more/setting/ResultActivity.kt
+++ more/src/main/java/com/example/more/setting/ResultActivity.kt
@@ -27,19 +27,9 @@ class ResultActivity : BaseActivity<MoreActivitySettingResultBinding, BaseViewMo
         addClickEvent()
     }
 
-    fun addClickEvent(){
-        //默认点击隐藏标题选择弹窗
-        window.decorView.setOnClickListener {
-            binding.layoutPopWindow.apply {
-                if (visibility == View.VISIBLE) visibility = View.INVISIBLE
-            }
-        }
-
-        binding.layoutPopWindow.apply {
-            binding.tvChooseTitle.setOnClickListener {
-                visibility = if (visibility == View.VISIBLE) View.INVISIBLE
-                else View.VISIBLE
-            }
+    fun addClickEvent() {
+        binding.tvChooseTitle.setOnClickListener {
+            isPopWindowShow()
         }
 
         //复制标题点击
@@ -50,11 +40,13 @@ class ResultActivity : BaseActivity<MoreActivitySettingResultBinding, BaseViewMo
         //点击插入前瞻
         binding.tvInsertAndCachePreAnalyseHead.setOnClickListener {
             writeCache(binding.etAnalyseHead.text?.toString() ?: "请设置默认前缀")
-            binding.etPreAnalyse.setText(binding.etAnalyseHead.text.toString()
-                    + "，下面由我为大家带来" + left_team_name
-                    + "VS"
-                    + right_team_name
-                    + "的前瞻分析。")
+            binding.etPreAnalyse.setText(
+                binding.etAnalyseHead.text.toString()
+                        + "，下面由我为大家带来" + left_team_name
+                        + "VS"
+                        + right_team_name
+                        + "的前瞻分析。"
+            )
         }
 
         //点击复制前瞻
@@ -62,58 +54,72 @@ class ResultActivity : BaseActivity<MoreActivitySettingResultBinding, BaseViewMo
             copyTextToSystem(binding.etPreAnalyse.text.toString() ?: "前瞻为空")
         }
 
+        //点击复制最终结果
+        binding.etFinalAnswer.let {
+            it.setOnClickListener { _ ->
+                if (it.text.isEmpty()) {
+                    showToast(this, "结果为空")
+                    return@setOnClickListener
+                }
+                copyTextToSystem(it.text.toString())
+            }
+        }
+
         binding.tvLeftWin.setOnClickListener { chooseWinner(left_team_name) }
         binding.tvRightWin.setOnClickListener { chooseWinner(right_team_name) }
     }
 
     //选择胜方
-    fun chooseWinner(winner : String){
-        val tail : String = "\n" + "最后、比分预测\n"+ left_team_name+ "VS"+right_team_name + "，" + winner + "受让分胜。"
+    fun chooseWinner(winner: String) {
+        val tail: String =
+            "\n" + "最后、比分预测\n" + left_team_name + "VS" + right_team_name + "，" + winner + "胜。"
         binding.etFinalAnswer.setText(binding.etFinalAnswer.text.toString() + tail)
     }
 
     //分析前瞻默认前缀缓存数据的 存取
-    fun writeCache(cache : String){
-        this.getSharedPreferences(ANALYSE_HEAD_SHARED_PREFERENCE,MODE_PRIVATE).edit()
-            .putString(ANALYSE_HEAD_CACHE,cache).commit()
+    fun writeCache(cache: String) {
+        this.getSharedPreferences(ANALYSE_HEAD_SHARED_PREFERENCE, MODE_PRIVATE).edit()
+            .putString(ANALYSE_HEAD_CACHE, cache).commit()
     }
 
-    fun readCache() : String = getSharedPreferences(ANALYSE_HEAD_SHARED_PREFERENCE,MODE_PRIVATE).getString(ANALYSE_HEAD_CACHE,"请设置默认前缀") as String
+    fun readCache(): String =
+        getSharedPreferences(ANALYSE_HEAD_SHARED_PREFERENCE, MODE_PRIVATE).getString(
+            ANALYSE_HEAD_CACHE,
+            "请设置默认前缀"
+        ) as String
 
     override fun getLayoutId() = R.layout.more_activity_setting_result
 
 
-    fun initPopWindow(){
-        binding.layoutPopWindow.apply {
-            setOnClickListener {
-                if (visibility == View.VISIBLE) visibility = View.INVISIBLE
-            }
+    fun initPopWindow() {
+        //拦截弹窗点击
+        binding.layoutPopWindow.setOnClickListener {
+
         }
 
         binding.tvTitle1.apply {
-            setAVSBMode(this,"胜利将花落谁家？")
+            setAVSBMode(this, "胜利将花落谁家？")
         }
         binding.tvTitle2.apply {
-            setAVSBMode(this,"谁能卫冕荣耀？")
+            setAVSBMode(this, "谁能卫冕荣耀？")
         }
         binding.tvTitle3.apply {
-            setAVSBMode(this,"昔日格局是否会被重塑？")
+            setAVSBMode(this, "昔日格局是否会被重塑？")
         }
         binding.tvTitle4.apply {
-            setAComeifBcoulde(this,"来势汹汹","能否逆境封神？")
+            setAComeifBcoulde(this, "来势汹汹", "能否逆境翻盘")
         }
         binding.tvTitle5.apply {
-            setAComeifBcoulde(this,"携登顶之势而来","能否抗住压力？")
+            setAComeifBcoulde(this, "来者不善", "能否抗住压力")
         }
         binding.tvTitle6.apply {
-            setAComeifBcoulde(this,"如虎冲狼奔","谁能挺身而出？")
+            setAComeifBcoulde(this, "夺冠目标明确", "能否御敌于球场")
         }
         binding.tvTitle7.apply {
             apply {
                 text = "势均力敌！" + "A和B谁能登临宝座?"
                 setOnClickListener {
-                    binding.etTitle.setText("龙争虎斗！" + left_team_name + "和" +right_team_name + "谁能登临宝座？")
-
+                    binding.etTitle.setText("势均力敌！" + left_team_name + "和" + right_team_name + "谁能登临王座？")
                 }
             }
         }
@@ -121,40 +127,49 @@ class ResultActivity : BaseActivity<MoreActivitySettingResultBinding, BaseViewMo
             apply {
                 text = "难分高下！" + "A能否击败B?"
                 setOnClickListener {
-                    binding.etTitle.setText("难分高下！" + left_team_name + "能否击败" +right_team_name + "？")
+                    binding.etTitle.setText("难分高下！" + left_team_name + "能否击败" + right_team_name + "？")
 
                 }
             }
         }
         binding.tvTitle9.apply {
-            apply {
-                text = "多年宿敌赛场再逢！" + "这次谁能笑道最后？"
-                setOnClickListener {
-                    binding.etTitle.setText("多年宿敌赛场再逢！" + "这次谁能笑道最后？")
-                }
+            text = "老对手球场再逢！" + "这次谁能笑道最后？"
+            setOnClickListener {
+                binding.etTitle.setText("老对手球场再逢！" + "这次谁能笑道最后？")
             }
         }
         binding.tvTitle10.apply {
-            setAVSBMode(this,"是否会有惊喜发生？")
+            setAVSBMode(this, "结局可能超出预料？")
+        }
+        binding.tvTitleQuitChoose.setOnClickListener {
+            isPopWindowShow()
         }
     }
 
-    fun setAVSBMode(tvTitle : TextView,tail : String){
+    fun setAVSBMode(tvTitle: TextView, tail: String) {
         tvTitle.apply {
             text = "AvsB，" + tail
             setOnClickListener {
-                binding.etTitle.setText(left_team_name + "VS" +right_team_name + "，" + tail)
+                binding.etTitle.setText(left_team_name + "VS" + right_team_name + "，" + tail)
             }
         }
     }
 
     //A四字成语，B能否四字成语
-    fun setAComeifBcoulde(tvTitle : TextView,aWord: String,bWord : String){
+    fun setAComeifBcoulde(tvTitle: TextView, aWord: String, bWord: String) {
         tvTitle.apply {
-            text = "A+成语，B能否+成语"
+            text = "A" + aWord + "，B能否" + bWord + "？"
             setOnClickListener {
-                binding.etTitle.setText(left_team_name + aWord + "，" + right_team_name + bWord)
+                binding.etTitle.setText(left_team_name + aWord + "，" + right_team_name + bWord + "？")
             }
+        }
+    }
+
+    //隐藏弹窗
+    fun isPopWindowShow() {
+        binding.layoutPopWindow.apply {
+            visibility = if (visibility == View.VISIBLE) View.INVISIBLE
+            else View.VISIBLE
         }
     }
 }
