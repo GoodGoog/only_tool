de462ee064d732323496c760aa1621fed4e9f4cd
zhangqy <1213715120@qq.com>
2026-07-02 00:18:19 +0800

    优化界面

--- Home/src/main/java/com/example/main/HomeFragment.java
+++ Home/src/main/java/com/example/main/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.more.touch.TouchActivity;
 
 public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        // 加载fragment布局
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        // 在根布局中findViewById
+        TextView tvStart = rootView.findViewById(R.id.tv_start_leisu);
+        tvStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(requireContext(),TouchActivity.class);
+                startActivity(intent);
+            }
+        });
+        return rootView;
     }
 }
